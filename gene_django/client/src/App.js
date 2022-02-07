24520c141b5912f2fda74aee0f5a0d36d2e82955
Lieba Pil <pil.lieba@gmail.com>
2022-02-07 12:41:57 -0800

    did routes

--- gene_django/client/src/App.js
+++ gene_django/client/src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import {Route , Routes} from 'react-router-dom'
+import Home from './main/Home'
+import User from './main/User'
 
 function App() {
   return (
     <div className="App">
-     
+     <Routes>
+       <Route path="/" element={<User/>}/>
+       <Route path="/geneseq" element={<Home/>}/>
+     </Routes>
     </div>
   );
 }
---
+++ gene_django/client/src/main/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Home() {
+  return(
+   <div>
+       <p>hello</p>
+  </div>
+  )}
