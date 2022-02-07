bf06f983e45d4b0e15a9e0a085b985b38f21dfe1
Lieba Pil <pil.lieba@gmail.com>
2022-02-07 13:37:11 -0800

    did register

--- gene_django/client/src/main/User.jsx
+++ gene_django/client/src/main/User.jsx
@@ -1,8 +1,75 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
 
 export default function User() {
+
+    const [userNameReg, setUserNameReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
+
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+   
+    const register = async (e)=>{
+        e.preventDefault() 
+        const res = await axios.post(`http://localhost:8000/user/`,{
+            user_name: userNameReg,
+            password: passwordReg
+        })
+        console.log(res)
+    }
+
+
+   
+    
+
+
   return (
-  <div>
-      hello
+  <div className='register_form'>
+      <form className='registers' onSubmit={register}>
+          <label htmlFor='username'>User Name</label>
+          <input
+           name='username'
+           type='text'
+           placeholder='user name'
+           onChange={(e)=>{
+               setUserNameReg(e.target.value)
+           }}
+           />
+          <label htmlFor='password'>password</label>
+          <input 
+          name='password'
+          type='text'
+          placeholder='password'
+          onChange={(e)=>{
+            setPasswordReg(e.target.value)
+        }}
+          />
+        <button >Register</button>
+      </form>
+      <form className='login'>
+          <label htmlFor='username'>User Name</label>
+          <input
+           name='username'
+           type='text'
+           placeholder='user name'
+           onChange={(e)=>{
+            setUserName(e.target.value)
+        }}
+           />
+          <label htmlFor='password'>password</label>
+          <input 
+          name='password'
+          type='text'
+          placeholder='password'
+          onChange={(e)=>{
+            setPassword(e.target.value)
+        }}
+          />
+           <button>Login</button>
+      </form>
+      
   </div>
   )}
--- gene_django/gene_django/settings.py
+++ gene_django/gene_django/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'gene_django.urls'
