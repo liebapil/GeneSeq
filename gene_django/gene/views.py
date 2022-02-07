8e033e15d170808c7742d31e81020dd361d5681c
Lieba Pil <pil.lieba@gmail.com>
2022-02-07 10:47:29 -0800

    user view and url

--- gene_django/gene/views.py
+++ gene_django/gene/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import User
 
 # Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
