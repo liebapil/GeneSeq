279727ee6bf2b657db910b3a824a4405f78445c2
Lieba Pil <pil.lieba@gmail.com>
2022-02-08 07:25:12 -0800

    finished get for seq

--- gene_django/client/src/components/SeqRender.jsx
+++ gene_django/client/src/components/SeqRender.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function SeqRender(props) {
     return (
         <div >
-            <Link className='seq_link'>
+            <Link className='seq_link' to={`http://localhost:8000/gene/${props.id}`}>
                 <p>Gene Name: {props.gene_name}</p>
                 <p>Sequence one: {props.sequence_one}</p>
                 <p>Sequence two: {props.sequence_two}</p>
--- gene_django/client/src/components/Sequence.jsx
+++ gene_django/client/src/components/Sequence.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios'
+import SeqRender from './SeqRender';
 
 export default function Sequence() {
   const [geneName, setGeneName] = useState('')
   const [sequencesOne, setSequencesOne] = useState('')
   const [sequencesTwo, setSequencesTwo] = useState('')
   const [submit, setSubmit] = useState(false);
+  const [getSeq, setGetSeq] = useState([])
 
   const postGene = async (e) => {
     e.preventDefault()
@@ -17,10 +19,20 @@ export default function Sequence() {
     const res = await axios.post(`http://localhost:8000/gene/`, {
       gene_name: geneName,
       sequence_one: sequencesOne,
-      sequence_Two: sequencesTwo
+      sequence_two: sequencesTwo
     })
-    console.log(res)
+    renderSeq()
   }
+
+  const renderSeq = async() =>{
+    const res = await axios.get(`http://localhost:8000/gene/`)
+    setGetSeq(res.data)
+  }
+
+  useEffect(() => {
+
+    renderSeq()
+}, [])
 
   return (
     <div>
@@ -54,6 +66,17 @@ export default function Sequence() {
         />
         <button>Submit</button>
       </form>
+      <div className='get_seq'>
+        {getSeq.map((props, idx)=>{
+          return (<SeqRender
+          key={idx}
+          gene_name={props.gene_name}
+          sequence_one={props.sequence_one}
+          sequence_two={props.sequence_two}
+          />)
+        })}
+
+      </div>
 
     </div>
   );
