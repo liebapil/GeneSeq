cf4bd52616e0314fcee8dc94770a7e1b8cb34030
Lieba Pil <pil.lieba@gmail.com>
2022-02-08 13:27:31 -0800

    mutation is not working

--- gene_django/client/src/components/Mutation.jsx
+++ gene_django/client/src/components/Mutation.jsx
@@ -11,35 +11,70 @@ export default function Mutation(props) {
   const { id } = useParams()
 
   //get mutation 
-   const [mutation, getMutation] = useState('no mutation')
-   const [polar, getPolar] = useState('no polar')
-   const [protonate, getProtonate] = useState('no protonate')
-   const [submit, setSubmit] = useState(false);
+  const [mutation, setMutation] = useState('no mutation')
+  const [polar, setPolar] = useState('no polar')
+  const [protonates, setProtonates] = useState('no protonate')
+  const [submit, setSubmit] = useState(false);
 
 
-   const getGene = async ()=>{
+  const getGene = async () => {
+    const res = await axios.get(`http://localhost:8000/gene/${id}`)
+    setGeneName(res.data.gene_name)
+    setSequencesOne(res.data.sequence_one)
+    setSequencesTwo(res.data.sequence_two)
 
-     const res = await axios.get(`http://localhost:8000/gene/${id}`)
-     setGeneName(res.data.gene_name)
-     setSequencesOne(res.data.sequence_one)
-     setSequencesTwo(res.data.sequence_two)
-     
-   }
-   useEffect(() => {
-    getGene()
-}, [])
-
-  const postMutation = async () =>{
-    const res = await axios.post(`http://localhost:8000/mutation/`)
   }
-   
+  useEffect(() => {
+    getGene()
+    postMutation()
+  }, [])
+
+  /////mutation
+  const postMutation = async (e) => {
+    e.preventDefault()
+    const res = await axios.post(`http://localhost:8000/mutation/`, {
+      mutation: mutation,
+      hphob_hphil: polar,
+      protonate: protonates
+    })
+    console.log(res)
+  }
+
+
 
   return (
-  <div>
-    <p>Gene name: {gene.gene_name}</p>
-    <p>Sequence one: {gene.sequence_one} </p>
-    <p>Sequence Two: {gene.sequence_two}</p>
-
-
-  </div>)
+    <div>
+      <p>Gene name: {gene.gene_name}</p>
+      <p>Sequence one: {gene.sequence_one} </p>
+      <p>Sequence Two: {gene.sequence_two}</p>
+      <div>
+        <form onSubmit={postMutation}>
+          <input
+            name='mutation'
+            type='text'
+            placeholder='mutation'
+            onChange={(e) => {
+              setMutation(e.target.value)
+            }}
+          />
+          <input
+            name='polar'
+            type='text'
+            placeholder='polar'
+            onChange={(e) => {
+              setPolar(e.target.value)
+            }}
+          />
+          <input
+            name='protonate'
+            type='text'
+            placeholder='protonate'
+            onChange={(e) => {
+              setProtonates(e.target.value)
+            }}
+          />
+          <input className="submit-mutation" type="submit" />
+        </form>
+      </div>
+    </div>)
 }
