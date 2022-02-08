b33c7ee18e55b4c77bf3435cf14692c56917c645
Lieba Pil <pil.lieba@gmail.com>
2022-02-08 07:38:48 -0800

    started update for seq

--- gene_django/client/src/components/SeqRender.jsx
+++ gene_django/client/src/components/SeqRender.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { useState } from "react"
 
 export default function SeqRender(props) {
+    const [update, setUpdate] = useState(false)
+    const [showForm, toggleShowForm] = useState(false)
+    const [deleteSeq, setDeleteSeq] = useState('')
+
+    const handleupdate = async (e) =>{
+        e.preventDefault()
+    }
+
     return (
         <div >
-            <Link className='seq_link' to={`http://localhost:8000/gene/${props.id}`}>
+            <div>
+            {/* <Link className='seq_link' to={`http://localhost:8000/gene/${props.id}`}> */}
                 <p>Gene Name: {props.gene_name}</p>
                 <p>Sequence one: {props.sequence_one}</p>
                 <p>Sequence two: {props.sequence_two}</p>
-            </Link>
-
+            {/* </Link> */}
+                <button>Delete</button>
+                <button>Edit</button>
+            </div>
         </div>
     )
 }
