ee2e7ea0b1229d8de7dfbe3490a98237ce25a15d
Lieba Pil <pil.lieba@gmail.com>
2022-02-10 11:22:46 -0800

    finished the form

--- gene_django/client/src/components/MutationRender.jsx
+++ gene_django/client/src/components/MutationRender.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MutationRender(props) {
   return( 
-  <div>
+  <div className='the_mutations'>
       <p className='mutation_render'>Mutation: {props.mutation}</p>
       <p className='hydro_render'>hydrophobic and hydrophilic: {props.hphob_hphil}</p>
       <p className='protonate_render'>protonate: {props.protonate}</p>
